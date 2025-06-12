01cd17bec71d1c5390167ad7684a7d7e36b4f289
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2025-06-12 16:30:25 +0530

    some changes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import userRouter from './user.route.js';
 import connectToDatabase from './Database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+
 const app=express();
 
 app.use(express.json());   //to handle json request
@@ -14,7 +16,8 @@ app.use(cookieParser())
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
-app.use(errorMiddleware)
+app.use(errorMiddleware);
+app.use(arcjetMiddleware);
 
 app.get('/',(req,res)=>{
     res.send("Welcome to the subscription tracker api",)    
